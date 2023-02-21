ca45f1baba27c24e309d782ec095deb7dae0ed29
tomdbrophy <tombrophy@mac.com>
2023-02-21 22:25:29 +0000

    Week04 v1

---
+++ collatz.py
@@ -0,0 +1,18 @@
+# collatz.py
+# Asks user to enter a positive integer.
+# If integer is even, halves number.
+# If integer is odd multiply by 3 and add 1.
+# Author: Tom Brophy
+
+values = []
+value = int(input('Please enter a positive integer: '))
+
+while value != 1:
+    if (value % 2) == 0:
+        values.append(str(int(value)))
+        value = value/2
+    else:
+        values.append(str(int(value)))
+        value = (value*3)+1
+values.append(str(int(value)))
+print(' '.join(values))
